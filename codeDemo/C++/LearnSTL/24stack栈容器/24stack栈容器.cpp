6665a689a2b24e4b5bafa4b8e0593ce6a0586509
zhangbin <abin_z@163.com>
2022-09-29 23:32:27 +0800

    2022.09.29 学习STL中的stack栈容器和queue队列容器

---
+++ codeDemo/C++/LearnSTL/24stack栈容器/24stack栈容器.cpp
@@ -0,0 +1,41 @@
+﻿// 24stack栈容器.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+void test() {
+	// 符合先进后出的数据结构, 不存在遍历行为
+	stack<string> s;
+
+	s.push("xiaoming");
+	s.push("baidu");
+	s.push("tom");
+	s.push("jerry");
+	s.push("tony");
+
+	stack<string> s2(s);	// 拷贝构造
+
+	stack<string> s3;
+	s3 = s2;				// 赋值运算符
+
+	cout << "s.empty() = " << s.empty() << endl;
+	cout << "s.size() = " << s.size() << endl;
+	cout << "s.size() = " << s.top() << endl;
+
+	// 只要栈不为空
+	while (!s3.empty())
+	{
+		// 查看栈顶元素
+		cout << "栈顶元素: " << s3.top() << endl;	// 查看元素需要使用top()函数		
+		s3.pop();			// pop() 函数返回的是void
+	}
+}
+
+int main()
+{
+	test();
+	std::cout << "Hello World!\n";
+}
+
---
+++ codeDemo/C++/LearnSTL/25queue队列容器/25queue队列容器.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+class Person {
+public:
+	Person(string name, int age) :
+		m_name(name),
+		m_age(age)
+	{}
+public:
+	string m_name;
+	int m_age;
+};
+
+void test() {
+	queue<Person> q;		// 创建队列
+
+	Person p1("唐僧", 1000);
+	Person p2("孙悟空", 1000);
+	Person p3("猪八戒", 1000);
+	Person p4("沙师弟", 1000);
+
+	// 入列
+	q.push(p1);
+	q.push(p2);
+	q.push(p3);
+	q.push(p4);
+
+	cout << " size = " << q.size() << endl;
+
+	while (!q.empty())
+	{
+		cout << "对头 = {" << q.front().m_name << "," << q.front().m_age << "}" << endl;
+		cout << "对尾 = {" << q.back().m_name << "," << q.back().m_age << "}" << endl;
+
+		// 出队列
+		q.pop();
+	}
+	cout << " size = " << q.size() << endl;
+}
+
+int main()
+{
+	test();
+	std::cout << "Hello World!\n";
+}
